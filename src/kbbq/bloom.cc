7a66253ca24ce428eab9b17e986000fe46b0637a
Adam Orr <adamjorr@gmail.com>
2020-05-29 18:32:05 -0700

    rewrite find_longest_fix

--- src/kbbq/bloom.cc
+++ src/kbbq/bloom.cc
@@ -116,80 +116,44 @@ namespace bloom
 		return std::array<size_t,2>{{anchor_start, anchor_end}};
 	}
 
-	std::pair<std::vector<char>, int> find_longest_fix(std::string seq, const bloomary_t& t, int k){
-		//this function is probably not very efficient.
+	char get_next_trusted_char(const bloom::Kmer& kmer, const bloomary_t& trusted){
+		for(char c: {'A','C','G','T'}){
+			bloom::Kmer extra = kmer;
+			extra.push_back(c);
+			if(trusted[extra.hashed_prefix()].query(extra.get_query())){
+				return c;
+			}
+		}
+		return 0;
+	}
+
+	std::pair<std::vector<char>,int> find_longest_fix(std::string seq, const bloomary_t& trusted, int k){
 		bloom::Kmer kmer(k);
-		int best_l = 0;
 		std::vector<char> best_c;
-		char original_c;
-		size_t end = std::min(seq.length(), 2UL * k - 1);
-		original_c = seq[k-1];
-		for(const char* e : {"A","C","G","T"} ){
-			char d = *e;
-			if( d != original_c ){
-				seq[k-1] = d;
-				kmer.reset();
-				size_t i;
-				for(i = 0; i < end; ++i){
-					kmer.push_back(seq[i]);
-					if (kmer.size() >= k) { // we find a k-mer
-						if(!t[kmer.hashed_prefix()].query(kmer.get_hashed()>>PREFIXBITS)){
-							break; //if it's not trusted, end here
-						}
-					} else if (kmer.size() == 0 && i > 0){ //we ran into a non-ATCG character
+		size_t best_l;
+		for(char c: {'A','C','G','T'}){
+			if(c == seq[k-1]){continue;}
+			seq[k-1] = c;
+			kmer.reset();
+			size_t i;
+			for(i = 0; i < 2 * k - 1; ++i){
+				char n = i < seq.length() ? seq[i] : get_next_trusted_char(kmer, trusted);
+				if(n == 0){
+					break;
+				}
+				kmer.push_back(n);
+				if(i >= k){
+					if(!trusted[kmer.hashed_prefix()].query(kmer.get_query())){
 						break;
 					}
 				}
-				if(i > best_l){
-					best_l = i;
-					best_c.clear();
-					best_c.push_back(d);
-				} else if (i == best_l){
-					best_c.push_back(d);
-				}
 			}
-		}
-		if(best_l == seq.length() && best_c.size() > 1){ //we ran out of kmers to try and we have a tie
-			for(const char* e : {"A","C","G","T"} ){
-				char d = *e; //the fix character
-				if( d != original_c ){
-					seq = seq.substr(0, end); //reset str to original length
-					size_t original_len = seq.length();
-					kmer.reset(); //reset kmer
-					seq[k-1] = d;
-					size_t i;
-					for(i = (end >= k ? end - k : 0); i < 2 * k - 1; ++i){
-						int cur_seqlen = seq.length();
-						if(i < seq.length()){
-							kmer.push_back(seq[i]);
-							if(kmer.size() == 0){ //we had a non-N base somehow; this shouldn't happen
-								break;
-							}
-						} else {
-							//we have to come up with extra bases.
-							for(const char* ex : {"A","C","G","T"}){
-								char extra = *ex; //the extension character
-								bloom::Kmer extra_kmer = kmer; //copy the previous kmer
-								extra_kmer.push_back(extra);
-								if(t[extra_kmer.hashed_prefix()].query(extra_kmer.get_query())){
-									kmer.push_back(extra); // add character to end
-									seq.push_back(extra); //
-									break; //try to extend again
-								}
-							}
-						}
-						if(cur_seqlen == seq.length()){ //no extra bases were trusted
-							break; //exit extension for this fix
-						}
-					}
-					if(i > best_l){ //we extended the read
-						best_l = i;
-						best_c.clear();
-						best_c.push_back(d);
-					} else if (i == best_l){
-						best_c.push_back(d);
-					}
-				}
+			if(i > best_l){
+				best_c.clear();
+				best_c.push_back(c);
+				best_l = i;
+			} else if (i == best_l){
+				best_c.push_back(c);
 			}
 		}
 		return std::make_pair(best_c, best_l);
