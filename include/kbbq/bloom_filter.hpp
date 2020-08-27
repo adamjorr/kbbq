4e6f6f32a30fc426e3423afbcc8a0d28d39122f4
Adam Orr <adamjorr@gmail.com>
2020-08-26 22:30:41 -0700

    fix an oopsie

--- include/kbbq/bloom_filter.hpp
+++ include/kbbq/bloom_filter.hpp
@@ -289,7 +289,7 @@ public:
          bit_table_ = new cell_type[static_cast<std::size_t>(raw_table_size_)];
          std::copy(f.bit_table_,f.bit_table_ + raw_table_size_,bit_table_);
          salt_ = f.salt_;
-         std::copy(std::begin(f.patterns), std::end(f.patterns), patterns);
+         std::copy(std::begin(f.patterns), std::end(f.patterns), std::begin(patterns));
          std::cerr << "bloom_filter operator= called!" << std::endl;
       }
       return *this;
