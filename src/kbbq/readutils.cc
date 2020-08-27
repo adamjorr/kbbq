ffc2737b17b4d4d5823e0992359bd2c04c837fc9
Adam Orr <adamjorr@gmail.com>
2020-08-27 16:40:03 -0700

    ensure errors is 0 initialized

--- src/kbbq/readutils.cc
+++ src/kbbq/readutils.cc
@@ -57,7 +57,7 @@ namespace readutils{
 			rg_to_pu[this->rg] = rg; //when loaded from the header this is actually a PU 
 		}
 		this->second = bamrecord->core.flag & BAM_FREAD2; // 0x80
-		this->errors.resize(bamrecord->core.l_qseq);
+		this->errors.resize(bamrecord->core.l_qseq,0);
 	}
 	// if second is >1, that means infer.
 
